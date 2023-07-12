1975b5df0ad4fde21195b6fcfd708bfff05fdf5d
manuelmtzv <manuel.mtzv816@gmail.com>
2023-07-12 14:29:30 -0600

    feat: finish pt2 of profile card challenge~

--- challenge_profile_card/src/components/SkillIcon.jsx
+++ challenge_profile_card/src/components/SkillIcon.jsx
@@ -1,9 +1,23 @@
 import PropTypes from "prop-types";
 
 export default function SkillIcon({ skill, ...props }) {
+  let emoji;
+
+  switch (skill.level) {
+    case "beginner":
+      emoji = "🥱";
+      break;
+    case "intermediate":
+      emoji = "😐";
+      break;
+    case "advanced":
+      emoji = "😎";
+      break;
+  }
+
   return (
     <span style={{ backgroundColor: skill.color }} key={skill.id} {...props}>
-      {skill.name} {skill.emoji}
+      {skill.name} {emoji}
     </span>
   );
 }
