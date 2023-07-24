4c8d7c647f9c0a262c879884c3838310ffa38fe9
manuelmtzv <manuel.mtzv816@gmail.com>
2023-07-24 15:36:33 -0600

    feat: add reusable component to accordion exercise~

--- accordion/src/components/Accordion.jsx
+++ accordion/src/components/Accordion.jsx
@@ -1,11 +1,22 @@
 import PropTypes from "prop-types";
 import AccordionItem from "./AccordionItem";
+import { useState } from "react";
 
 export default function Accordion({ faqs }) {
+  const [actualFaq, setActualFaq] = useState(null);
+
   return (
     <div className="accordion">
       {faqs.map((faq, index) => (
-        <AccordionItem num={index + 1} faq={faq} key={faq.title} />
+        <AccordionItem
+          num={index + 1}
+          faq={faq}
+          open={actualFaq === index + 1}
+          setActualFaq={setActualFaq}
+          key={faq.title}
+        >
+          {faq.text}
+        </AccordionItem>
       ))}
     </div>
   );
--- accordion/src/components/AccordionItem.jsx
+++ accordion/src/components/AccordionItem.jsx
@@ -1,24 +1,31 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
 
-export default function AccordionItem({ num, faq }) {
-  const [open, setOpen] = useState(false);
+export default function AccordionItem({
+  num,
+  faq,
+  open,
+  setActualFaq,
+  children,
+}) {
+  function handleClick() {
+    setActualFaq(open ? null : num);
+  }
 
   return (
-    <div
-      className={`item ${open ? "open" : ""}`}
-      onClick={() => setOpen((prev) => !prev)}
-    >
+    <div className={`item ${open ? "open" : ""}`} onClick={handleClick}>
       <p className="number">{num < 10 ? `0${num}` : num}</p>
       <p className="title">{faq.title}</p>
       <p className="icon">{open ? "−" : "+"}</p>
 
-      {open && <div className="content-box">{faq.text}</div>}
+      {open && <div className="content-box">{children}</div>}
     </div>
   );
 }
 
 AccordionItem.propTypes = {
   num: PropTypes.number.isRequired,
+  open: PropTypes.bool.isRequired,
   faq: PropTypes.object.isRequired,
+  setActualFaq: PropTypes.func.isRequired,
+  children: PropTypes.node,
 };
