f445fd3940687dc257c867a2850e4fe00ab845d7
manuelmtzv <manuel.mtzv816@gmail.com>
2023-09-15 15:11:35 -0600

    feat: add useEffect to fetch movie data

--- usepopcorn/src/App.jsx
+++ usepopcorn/src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { tempMovieData } from "./data/tempMovieData";
+import { useEffect, useState } from "react";
 import { tempWatchedData } from "./data/tempWatchedData";
 
 import Main from "./components/Main";
@@ -13,10 +12,28 @@ import WatchedList from "./components/WatchedList";
 import ListBox from "./components/ListBox";
 import WatchedBox from "./components/WatchedBox";
 
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+
 export default function App() {
   const [query, setQuery] = useState("");
-  const [movies] = useState(tempMovieData);
+  const [movies, setMovies] = useState([]);
   const [watched] = useState(tempWatchedData);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`
+      );
+
+      const data = await res.json();
+
+      if ("Search" in data) {
+        setMovies(data.Search);
+      }
+    }
+
+    fetchData();
+  }, [query]);
 
   return (
     <>
--- usepopcorn/src/main.jsx
+++ usepopcorn/src/main.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-// import './index.css'
+import App from "./App.jsx";
+import "./index.css";
 
-import StarRating from "./components/star/StarRating.jsx";
+// import StarRating from "./components/star/StarRating.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
 
-    <StarRating
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
       defaultRating={5}
-    />
+    /> */}
   </React.StrictMode>
 );
