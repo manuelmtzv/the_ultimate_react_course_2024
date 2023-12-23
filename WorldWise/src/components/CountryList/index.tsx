e2421f8123b3b7c1b904a1b81e6e5c4fd6872745
manuelmtzv <manuel.mtzv816@gmail.com>
2023-12-22 18:21:45 -0600

    feat: advances on worldwise project

--- WorldWise/src/components/CountryList/index.tsx
+++ WorldWise/src/components/CountryList/index.tsx
@@ -3,14 +3,23 @@ import Spinner from "../Spinner";
 import CityEntry from "../CityEntry";
 import Message from "../Message";
 import { City } from "@/interfaces/city";
+import { objectOmit } from "@vueuse/core";
 
 export default function CountryList({ cities, isLoading }: Props) {
   if (isLoading) return <Spinner />;
 
   if (!cities.length && !isLoading)
     return (
-      <Message message="Add your first city by clicking on a city on the map" />
+      <Message message="Add your first city by clicking on a country on the map" />
     );
+
+  const countries = cities.reduce((acc, cur) => {
+    if (acc.map(el => el.city).includes(cur.country)) {
+      return acc;
+    } else {
+      return [...acc, cur];
+    }
+  }, [])
 
   return (
     <ul className={styles.countryList}>
